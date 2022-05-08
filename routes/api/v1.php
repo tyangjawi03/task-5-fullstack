49df63e32398772ee20c161694a2a7e76b28f2a4
tyangjawi03 <tyangjawi03@users.noreply.github.com>
2022-05-08 22:49:12 +0700

    add version on the api (#19)

---
+++ routes/api/v1.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\CategoryController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::apiResources([
+    'categories' => CategoryController::class,
+    'posts' => PostController::class
+], [
+    'middleware' => 'auth:api'
+]);
